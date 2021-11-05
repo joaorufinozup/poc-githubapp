96f9197470ed1ff1129fa7012c03c7ea86a69a45
JoaoDanielRufino <joaodaniel0405@gmail.com>
2021-11-05 15:34:00 -0300

    env vars
    Signed-off-by: JoaoDanielRufino <joaodaniel0405@gmail.com>

--- githubapp.js
+++ githubapp.js
@@ -1,6 +1,7 @@
 const { createAppAuth } = require("@octokit/auth-app");
 const { Octokit } = require("@octokit/rest");
 const fs = require("fs");
+require("dotenv").config();
 
 const getOctokit = () => {
   const pem = fs.readFileSync("./jd-poc-2.2021-11-03.private-key.pem", "utf8");
@@ -8,11 +9,11 @@ const getOctokit = () => {
   return new Octokit({
     authStrategy: createAppAuth,
     auth: {
-      appId: 149373,
+      appId: Number(process.env.APP_ID),
       privateKey: pem,
-      clientId: "Iv1.ca365a2d1513307a",
-      clienteSecret: "d080e5ca5a27eccbc312c3356e972eb626083d42",
-      installationId: 20485655,
+      clientId: process.env.CLIENT_ID,
+      clienteSecret: process.env.CLIENTE_SECRET,
+      installationId: Number(process.env.INSTALLATION_ID),
     },
   });
 };
